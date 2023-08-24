c6f11389d7bbc4e8695bdaab13e3b2bccf2df35e
JustSomeBeans <102830580+JustSomeBeans@users.noreply.github.com>
2023-08-24 12:37:10 -0700

    Update 3 Lab 1.c
    Adds more comments

--- Lab 1.c
+++ Lab 1.c
@@ -12,20 +12,20 @@ int main() {
 
         printf("Enter starting population \n");
         scanf("%d", &population); //storing input
-        do {
+        do {  //handles blank space in user input
             population = getchar();
         } while (isspace(runAgain));
 
         printf("Enter target population \n");
         scanf("%d", &populationEnd); //storing input
 
-        if (population > populationEnd) {  //checks invalid inputs and returns error if input is invalid
+        if (population > populationEnd) {  //checks invalid inputs and restarts if error is invalid
             printf("Invalid input, target population is smaller than starting population \n");
-            sleep(1);
-            break;
+            runAgain = 'y'; //sets the loop to a value where it will run again.
+            goto exitLoop; //Used to avoid unnecessary nesting of loops. Brings the code to the end of the loop. exitLoop is on line 59
         }
         do {  //loop for finding number of iterations necessary to reach target population
-            population = round(population - (population / 4) + (population / 3));
+            population = round(population - (population / 4) + (population / 3));  //specified population growth by LAb 1
             count++;
         }  while(population < populationEnd); //loop condition
 
@@ -34,10 +34,10 @@ int main() {
 
         printf("Do you want to run another instance? (y/n) \n");
         scanf("%c", &runAgain);
-        do {
+        do {  //handles blank space in user input
             runAgain = getchar();
         } while (isspace(runAgain));
-        switch (runAgain) {
+        switch (runAgain) { //if the user does not want to run another instance, checks for runAgain to be n and exits the program
             case 'n':
                 printf("terminating instance");
                 sleep(1);
@@ -48,16 +48,16 @@ int main() {
                 sleep(1);
                 exit(0);
                 break;
-            case 'y':
+            case 'y': //ensuring default does not trigger if runAgain is y
                 break;
             case 'Y':
                 break;
             default:
-                printf("Invalid input, continuing by default \n");
+                printf("Invalid input, continuing by default \n");  //by default runs the program again.
                 runAgain = 'y';
         }
-
-    } while (runAgain == 'y' || runAgain == 'Y');
+    exitLoop:
+    } while (runAgain == 'y' || runAgain == 'Y'); //condition for restarting the loop.
 
     return 0;
 }
